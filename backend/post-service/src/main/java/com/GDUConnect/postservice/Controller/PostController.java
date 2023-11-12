c015873d149ce46dc95a31f875c288622c8fe793
Hoai2801 <dreamhoaihack@gmail.com>
2023-11-12 21:50:46 +0700

    circuit breaker

--- backend/post-service/src/main/java/com/GDUConnect/postservice/Controller/PostController.java
+++ backend/post-service/src/main/java/com/GDUConnect/postservice/Controller/PostController.java
@@ -1,24 +1,34 @@
 package com.GDUConnect.postservice.Controller;
 
+import com.GDUConnect.postservice.DTO.PostDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("api/v1/post")
 @RequiredArgsConstructor
 public class PostController {
     public final WebClient.Builder webClientBuilder;
-    @GetMapping("")
-    public ResponseEntity<String> getPosts() {
-        String userId = webClientBuilder.build().get()
-                .uri("http://user-service/api/v1/user")
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-        return ResponseEntity.ok().body("Get Post Successfully of " + userId);
+
+    @PostMapping("")
+    @CircuitBreaker(name = "user", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "user")
+    @Retry(name = "user")
+    public CompletableFuture<ResponseEntity<String>> getPosts(@RequestBody PostDTO postDTO) {
+        return CompletableFuture.supplyAsync(() -> {
+            String user = webClientBuilder.build().get().uri("http://user-service/api/v1/user").retrieve().bodyToMono(String.class).block();
+            return ResponseEntity.ok().body(postDTO.getTitle() + " " + user);
+        });
+    }
+
+    public CompletableFuture<String> fallbackMethod(@RequestBody PostDTO postDTO, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Opps! User cannot be found, try again!");
     }
 }
---
+++ backend/post-service/src/main/java/com/GDUConnect/postservice/DTO/PostDTO.java
@@ -0,0 +1,12 @@
+package com.GDUConnect.postservice.DTO;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDTO {
+    String title;
+}
