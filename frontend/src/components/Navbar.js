a33aec7cf666ff91a9868101e1109567957e2b3c
AlexNe <truonggiang190689@gmail.com>
2023-11-21 01:24:32 +0700

    them font vo html, them scss, code navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+function Nav() {
+  return (
+    <>
+      <a href="/">Bài Đăng</a>
+      <a href="/room">Phòng Trọ</a>
+      <a href="/shop">Buôn Bán</a>
+      <a href="/competition">Cuộc Thi</a>
+    </>
+  );
+}
 
 const Navbar = () => {
   return (
-    <div>Navbar -> bài đăng, phòng trọ, buôn bán, cuộc thi, user chưa đăng nhập và đã đăng nhập</div>
-  )
-}
+    <>
+      <div className="navbar">
+        <h1 className="logo">GDUConnect</h1>
+        <div className="middle-section">
+          <Nav />
+        </div>
+        <div className="right-section">
+          <span class="material-symbols-outlined">menu</span>
+          <button type="button">
+            <a href="#">Đăng Nhập</a>
+          </button>
+        </div>
+      </div>
+      <div className="sidebar">
+        <span class="material-symbols-outlined">close</span>
+        <Nav />
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
