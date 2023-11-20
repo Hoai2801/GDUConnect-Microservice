b3e14e874c4bee1b8167cd22204a7256fa83ea40
Hoai2801 <dreamhoaihack@gmail.com>
2023-11-20 19:53:29 +0700

    frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Room from './pages/Room';
+import Shop from './pages/Shop';
+import Competition from './pages/Competition';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Layout />} >
+        <Route path='/' element={<p>Home</p>} />
+        <Route path='/room' element={<Room />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/competition' element={<Competition />} />
+        {/* <Route path='/room' element={<Room />} /> */}
+      </Route>
+    </Routes>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar -> bài đăng, phòng trọ, buôn bán, cuộc thi, user chưa đăng nhập và đã đăng nhập</div>
+  )
+}
+
+export default Navbar
